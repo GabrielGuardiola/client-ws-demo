37f70722019a023fb828280573e064de266c04ae
Gguardiola <gabriel.guardiola1999@gmail.com>
2024-03-20 11:02:45 +0100

    init commit

---
+++ src/main/java/com/gabodev/clientwsdemo/ClientWsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gabodev.clientwsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientWsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientWsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabodev/clientwsdemo/ClientWsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabodev.clientwsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientWsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
